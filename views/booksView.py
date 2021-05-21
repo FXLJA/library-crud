0c3b94b7773c90987da622e18382904f9e354acd
FXLJA <ja669543@gmail.com>
2021-05-21 23:04:59 +0700

    fix book crud

--- views/booksView.py
+++ views/booksView.py
@@ -5,29 +5,27 @@ from flask import Blueprint
 from flask import render_template
 
 from controllers.bookController import BookController
+from models.book import Book
+from controllers.categoryController import CategoryController
 
 
 blueprint = Blueprint("books", __name__, url_prefix="/books")
-bc = BookController()
 
 
 @blueprint.route('/add', methods=['GET', 'POST'])
 def add():
+    list_category = CategoryController.getAll()
     if request.method == 'POST':
-        book_id = request.form['book_id']
-        title = request.form['title']
-        author = request.form['author']
-        book_category = request.form['book_category']
+        b = Book(*request.form.values())
 
-        if bc.getByID(book_id) is not None:
-            return render_template('views/addBooks.html', message='ID already exists!')
+        if BookController.getByID(b.book_id) is not None:
+            return render_template('book/addBooks.html', message='ID already exists!', list_category=list_category)
 
-        b = Book(book_id, title, author, book_category)
-        bc.insert(b)
+        BookController.insert(b)
 
         return redirect(url_for('index'))
     else:
-        return render_template('views/addBooks.html')
+        return render_template('book/addBooks.html', list_category=list_category)
 
 
 @blueprint.route('/view')
@@ -37,15 +35,16 @@ def view():
 
 @blueprint.route('/update', methods=['GET', 'POST'])
 def update():
+    list_category = CategoryController.getAll()
     if request.method == "POST":
         book_id = request.form['book_id']
-        b = bc.getByID(book_id)
+        b = BookController.getByID(book_id)
 
         if b is None:
-            return render_template('views/updateBooks.html', message="Book not found")
+            return render_template('book/updateBooks.html', message="Book not found")
 
-        return render_template('views/UpdateConfirm.html', book=b)
-    return render_template('views/updateBooks.html')
+        return render_template('book/UpdateConfirm.html', book=b, list_category=list_category)
+    return render_template('book/updateBooks.html')
 
 
 @blueprint.route('/update_confirm', methods=['POST'])
@@ -56,7 +55,7 @@ def update_confirm():
     book_category = request.form['book_category']
 
     b = Book(book_id, title, author, book_category)
-    bc.update(b)
+    BookController.update(b)
 
     return redirect(url_for('index'))
 
@@ -65,11 +64,11 @@ def update_confirm():
 def delete():
     if request.method == "POST":
         book_id = request.form['book_id']
-        b = bc.getByID(book_id)
+        b = BookController.getByID(book_id)
 
         if b is None:
-            return render_template('views/deleteBooks.html', message="Book not found")
+            return render_template('book/deleteBooks.html', message="Book not found")
 
-        bc.delete(b.book_id)
+        BookController.delete(b.book_id)
         return redirect(url_for('index'))
-    return render_template('views/deleteBooks.html')
+    return render_template('book/deleteBooks.html')
